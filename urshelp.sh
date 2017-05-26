f53574703e3bee5f769064e806c6fbbc9b6acda1
Ramakrishna Boggarapu <ramab@us.ibm.com>
2017-05-26 14:11:31 -0400

    open up 10255 port

--- urshelp.sh
+++ urshelp.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-grep "^Domain = slnfsv4.com" /etc/idmapd.conf
-if [ "$?" -ne "0" ] ; then
-        sed -i 's/.*Domain =.*/Domain = slnfsv4.com/g' /etc/idmapd.conf
-        apt-get update
-        apt-get install -y nfs-common nfs-kernel-server
-        nfsidmap -c
-        service nfs-idmapd start
-fi
-
+/sbin/iptables -I INPUT 1 -p tcp --dport 10255 -j ACCEPT
